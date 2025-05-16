f4f242fd2c11b56a849bf87309c083479e3c8ff4
mauricio-gallera <galmeidamauricio12@gmail.com>
2025-05-16 18:52:09 -0300

    add login (WIP)

--- src/main/java/com/ufscar/projectmanager/controllers/HelloController.java
+++ src/main/java/com/ufscar/projectmanager/controllers/HelloController.java
@@ -3,6 +3,9 @@ package com.ufscar.projectmanager.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
@@ -10,6 +13,13 @@ public class HelloController {
     @GetMapping("/")
     public String hello(Model model) {
         return "hello";
+    }
+
+    @PostMapping("/login")
+    public ModelAndView login(@RequestParam("name") String name) {
+        ModelAndView mv = new ModelAndView("redirect:/projects");
+        mv.addObject("name", name);
+        return mv;
     }
 
 }
